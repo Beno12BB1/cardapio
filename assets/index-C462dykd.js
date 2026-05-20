eca1553f5632d1fe385863188ad85c8923e5ac6e
Breno Rodrigues e julia dias <sgamedbr@gmail.com>
2026-05-20 19:55:34 -0300

    Updates

---
+++ assets/index-C462dykd.js
@@ -0,0 +1,1 @@
+import{v as o}from"./auth-BFs9X2rx.js";const i="/cardapio/";o().then(a=>{a&&(window.location.href=i+"pages/dashboard.html")});
--- assets/index-D8zUMurA.js
+++
@@ -1,1 +0,0 @@
-import{v as o}from"./auth-BWDIw5O6.js";const i="/cardapio/";o().then(a=>{a&&(window.location.href=i+"pages/dashboard.html")});
